97b3cce92ea53dd7caca4de9da779cfba9cbf09d
Sana Yoosuf <sanazaier777@gmail.com>
2024-05-18 07:42:46 +0400

    Form Validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,15 +1,70 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Register() {
-  return <section className="container">
-    <form className="form">
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    age: "age",
+  });
+
+  function handleInput(event) {
+    setUserDetails((prevState)=>{
+      return{...prevState,[event.target.name]:event.target.value}
+    })
+  }
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(userDetails)
+
+  }
+  return (
+    <section className="container">
+      <form className="form" onSubmit={handleSubmit}>
         <h1>Start Your Fitness</h1>
-        <input className="inp" type="text" placeholder="Enter Name" name="name"/>
-        <input className="inp" type="email" placeholder="Enter Email" name="email"/>
-        <input className="inp" type="password" placeholder="Enter Password" name="password"/>
-        <input className="inp" type="number" placeholder="Enter Age" name="age"/>
+        <input
+          className="inp"
+          type="text"
+          placeholder="Enter Name"
+          name="name"
+          required
+          onChange={handleInput}
+          value={userDetails.name}
+        />
+        <input
+          className="inp"
+          type="email"
+          required
+          placeholder="Enter Email"
+          name="email"
+          onChange={handleInput}
+          value={userDetails.email}
+        />
+        <input
+          className="inp"
+          type="password"
+          placeholder="Enter Password"
+          required maxLength={8}
+          name="password"
+          onChange={handleInput}
+          value={userDetails.password}
+        />
+        <input
+          className="inp"
+          type="number"
+          placeholder="Enter Age"
+          required 
+          name="age"
+          max={100} min={12}
+          onChange={handleInput}
+          value={userDetails.age}
+        />
         <button className="btn">Join</button>
-        <p>Already Registered ? <Link to='/login' >Login</Link> </p>
-    </form>
-  </section>;
+        <p>
+          Already Registered ? <Link to="/login">Login</Link>{" "}
+        </p>
+      </form>
+    </section>
+  );
 }
